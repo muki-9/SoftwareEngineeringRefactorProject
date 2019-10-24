ac14967518b0b9a8d68e10bd5836e7172f5935d8
raghibmrz <zcabrmi@ucl.ac.uk>
2019-10-24 13:14:50 +0100

    First push from Raghib

--- src/test/java/uk/ac/ucl/jsh/JshTest.java
+++ src/test/java/uk/ac/ucl/jsh/JshTest.java
@@ -3,6 +3,7 @@ package uk.ac.ucl.jsh;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
+import java.io.IOException;
 import java.io.PipedInputStream;
 import java.io.PipedOutputStream;
 import java.util.Scanner;
@@ -12,13 +13,32 @@ public class JshTest {
     }
 
     @Test
-    public void testJsh() throws Exception {
+    public void testEcho() throws Exception {
+        Jsh echoShell = new Jsh();
         PipedInputStream in = new PipedInputStream();
-        PipedOutputStream out;
-        out = new PipedOutputStream(in);
-        Jsh.eval("echo foo", out);
+        PipedOutputStream out = new PipedOutputStream(in);
+        echoShell.eval("echo foo", out);
         Scanner scn = new Scanner(in);
-        assertEquals(scn.next(),"foo");
+        assertEquals(scn.next(), "foo");
+        scn.close();
+    }
+
+    public void testPwd() throws IOException {
+        Jsh pwdShell = new Jsh();
+        PipedInputStream in = new PipedInputStream();
+        PipedOutputStream out = new PipedOutputStream(in);
+        pwdShell.eval("pwd", out);
+        Scanner scn = new Scanner(in);
+        assertEquals(scn.next(), "pwd");
+        scn.close();
+    }
+
+    public void testCd() throws IOException {
+        Jsh cdShell = new Jsh();
+        PipedInputStream in = new PipedInputStream();
+        PipedOutputStream out = new PipedOutputStream(in);
+        cdShell.eval("cd", out);
+
     }
 
 }
