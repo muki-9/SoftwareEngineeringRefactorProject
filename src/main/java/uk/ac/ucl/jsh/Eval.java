a4f1f2c1a04460d2299accf0db7494ee6f4b9828
raghibmrz <zcabrmi@ucl.ac.uk>
2019-12-03 11:39:14 +0000

    created eval class- need to implement factory pattern

---
+++ src/main/java/uk/ac/ucl/jsh/Eval.java
@@ -0,0 +1,22 @@
+package uk.ac.ucl.jsh;
+
+public class Eval implements CommandVisitor {
+
+    @Override
+    public void visit(Pipe pipe) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void visit(Seq seq) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void visit(Call call) {
+        //implement factory pattern then complete this section
+    }
+
+}
